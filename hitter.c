08b287f992c024dadab5ce0f40cfe92d6d957795
Vinayak Singh <volcof@localhost.localdomain>
2026-04-03 01:08:23 +0530

    Added arg parse

--- hitter.c
+++ hitter.c
@@ -2,6 +2,12 @@
 #include <signal.h>
 #include <stdlib.h>
 #define r0 return 0
+typedef double DBL;
+
+struct Threat{
+    DBL X0, Y0, Z0;
+    DBL VX0, VY0, VZ0;
+};
 
 void problem(int sig){
     puts("Something went wrong.");
@@ -10,6 +16,15 @@ void problem(int sig){
 
 int main(int ARGC, char* ARG[]){
     signal(SIGSEGV, problem);
-
+    struct Threat T;
+    if (ARGC != 7){
+        printf("Needed 7 args, got %i instead.\n",ARGC);
+        raise(SIGTERM);
+    T.VX0 = atof(ARG[1]);
+    T.VY0 = atof(ARG[2]);
+    T.VZ0 = atof(ARG[3]);
+    T.X0 = atof(ARG[4]);
+    T.Y0 = atof(ARG[5]);
+    T.Z0 = atof(ARG[6]);
     r0;
 }
